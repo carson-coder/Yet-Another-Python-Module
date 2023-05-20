7ad9c2b1275f7e79993346461093ebb08a8999d3
carson-coder <carsondpool@gmail.com>
2023-05-20 08:44:52 -0400

    add files for pypi and fix some stuff

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+import json
+data = json.load(open('./config.json'))
+setup(
+  name='YetAnotherModule',
+  version=f"{data['version']}",
+  description='Yet Another Python Module.',
+  author="Carson",
+  author_email="carsondpool@gmail.com",
+  url='https://github.com/carson-coder/Yet-Another-Python-Module',
+  license="LICENSE",
+  long_description=open('README.md').read(),
+  packages=['YAPM'],
+  py_modules=['collections.abc',]
+)
